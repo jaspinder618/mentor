73b597c36a56d0b5bcb4359a8bc78d86c514b476
unknown <jaspinder618>
2023-06-22 12:07:25 +0530

    first

--- mentor/settings.py
+++ mentor/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 import os 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+STATIC_ROOT = os.path.join(BASE_DIR,'app/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
